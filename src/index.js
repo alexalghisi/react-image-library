59e9b903477e96d66891a0002fec66ac7cceb6b9
Alghisi Alessandro <alexalghisi@gmail.com>
2024-12-02 08:32:27 +0200

    Clean code

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import "./styles.css";
 
 const srcs = [
   "https://avatars.githubusercontent.com/u/10101138?v=4",
-  "https://images.unsplash.com/photo-1573430365123-1ba5faa6b486?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
-  "https://images.unsplash.com/photo-1572724013549-0f11f2a52657?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80"
+  "https://ziardecluj.ro//wp-content/uploads/2022/02/alessandro_2_0.jpg",
+  "http://www.ziardecluj.ro/sites/default/files/styles/large/public/media/image/2015/10/alessandro_1_0.jpg?itok=pUyPFbwi"
 ];
 
 const useSrcs = () => {
